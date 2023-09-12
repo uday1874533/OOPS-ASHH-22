6b143833ccdbd0b6827e3a28715e9ecf79b78979
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-09-12 11:44:42 +0530

    Add files via upload

---
+++ DIFFSUM.cpp
@@ -0,0 +1,48 @@
+// TENSION ON PRESENTATION BUT SOLVED
+// EAAT 5 * DO NOTHING
+#pragma GCC optimize("O2,no-stack-protector,unroll-loops,fast-math")
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+#define PB push_back
+#define F first
+#define S second
+#define uset unordered_set
+#define umap unordered_map
+#define MP make_pair
+#define vt vector
+#define all(x) begin(x), end(x)
+#define sz(x) (int)x.size()
+#define REP(i, a) for (int i = 0; i < a; i++)
+#define pii pair<int, int>;
+#define pll pair<ll, ll>;
+
+using namespace std;
+
+typedef vector<ll> vl;
+typedef pair<ll,ll> pl;
+
+void solve()
+{
+    int a,b;
+    cin>>a>>b;
+    if (a>b) {
+        cout<<a-b<<endl;
+    }
+    else{ 
+        cout << a+b<<endl;
+    }
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+   	ll t;
+    t=1;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}   
